f56e72098b6aefdbf88fdd5106d36ab6fe225e60
Francesco Gramano <fgramano@kpd-i.com>
2017-10-22 13:12:11 -0400

    wip(develop): adds meetup-api as dependency and dotenv for config management

--- backend/index.js
+++ backend/index.js
@@ -3,4 +3,14 @@ import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
 import bodyParser from 'body-parser';
+import MeetupAPI from 'meetup-api';
 
+const meetup = MeetupAPI({
+    key: process.env.MEETUP_API_KEY
+});
+
+/*
+meetup.getStreamOpenEvents(parameters, function(err, resp) {
+    console.log(err, resp);
+});
+*/
